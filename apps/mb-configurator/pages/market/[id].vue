15c3a9d1098f27bd2ef9b6fd1d711289429402c6
Sergei Zheleznov <sergei.zheleznov@outlook.com>
2023-10-11 00:48:09 +0200

    introduce market route

---
+++ apps/mb-configurator/pages/market/[id].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { MarketItem } from "~/types";
+
+const { params } = useRoute();
+const { id } = params;
+
+const { data } = await useFetch<MarketItem[]>(`/api/markets/${id}`);
+</script>
+<template>
+  <div>-- {{ JSON.stringify(data) }}</div>
+</template>
